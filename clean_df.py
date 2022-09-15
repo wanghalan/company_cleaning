b523f97916d6b61e6e4a378f27aa6bed26cd96ac
alan <ahw9f@virginia.edu>
2022-09-15 02:01:40 -0400

    actually pretty good updates

--- clean_df.py
+++ clean_df.py
@@ -1,3 +1,4 @@
+from tracemalloc import start
 import pandas as pd
 from tqdm import tqdm
 import re
@@ -5,6 +6,7 @@ import nltk
 from nltk.corpus import words
 import itertools
 from fuzzywuzzy import fuzz
+from pprint import pprint
 
 # nltk.download('words')
 
@@ -27,7 +29,9 @@ def filter_company_name(cleaned_company_name):
         "holdings",
         "son",
         "co",
+        "corporation",
         "inc",
+        "incorporated",
         "brothers",
         "tech",
         "foods",
@@ -37,6 +41,7 @@ def filter_company_name(cleaned_company_name):
         "bureau",
         "prestige",
         "worldwide",
+        "international",
         "direct",
         "online",
         "properties",
@@ -52,6 +57,7 @@ def filter_company_name(cleaned_company_name):
         "brands",
         "logistics",
         "companies",
+        "company",
         "creative",
         "productions",
         "prod",
@@ -77,10 +83,17 @@ def filter_states_info(cleaned_company_name, states):
         return cleaned_company_name
 
 
-def non_normal_majority_merging(company_a, company_b):
+def check_same_name(company_a, company_b):
     """
     For each token in the company name, if both companies have character remains after filtering for normal words, if the intersection of non-english words is > 0 return True
     """
+
+    # check for subset of string space removed
+    if "".join(company_a.split()) in "".join(company_b.split()) or "".join(
+        company_b.split()
+    ) in "".join(company_a.split()):
+        return True
+
     d = set(words.words())
     a = set(company_a.split(" "))
     b = set(company_b.split(" "))
@@ -90,55 +103,94 @@ def non_normal_majority_merging(company_a, company_b):
 
     # print(remain_a)
     # print(remain_b)
+    answer = False
 
-    if len(remain_a) <= 0 or len(remain_b) <= 0:
-        return False, None
-    else:
-        # check for intersection. If the total words in the intersection characters is greater than the mean of the disjoint character sum, return True
+    # if either company retains words after you subtract the normal dictionary
+    if len(remain_a) > 0 and len(remain_b) > 0:
         intersect = remain_a.intersection(remain_b)
-        disjoint = remain_a ^ remain_b
+        print("\t\t[%s]: %s" % (len(intersect), intersect))
+        print(remain_a)
+        print(remain_b)
+        print(remain_a ^ remain_b)
+        # if size of the intersection is the whole word for at least one of the company names, return True
+        if len(intersect) > 0:
+            answer = True
+        elif "".join(remain_a) in "".join(remain_b) or "".join(remain_b) in "".join(
+            remain_a
+        ):
+            answer = True
 
-        int_s = "".join(intersect)
-        dis_s = "".join(disjoint)
-        #         print(intersect)
-        #         print('intersect: %s' % int_s)
-        #         print('disjoint: %s' % dis_s)
-        if len(int_s) > len(dis_s) and len(dis_s) == 0:
-            return True, min([company_a, company_b], key=len)
-        else:
-            return False, None
+    return answer
 
 
 if __name__ == "__main__":
     print(filter_company_name("coca cola co"))
     print(filter_company_name("coca cola"))
+    print(filter_company_name("great lakes brewing company"))
+    print(filter_company_name("boston bears co"))
     states_df = prep_states_info()
     states = itertools.chain(
         states_df["State"], states_df["Standard"], states_df["Postal"]
     )
     print(filter_states_info("coca cola fl", states))
 
-    df = pd.read_csv("QnA_Labeled_Food_Companies_2017_companies.csv")
+    df = pd.read_csv("QnA_Labeled_Food_Companies_2017_companies_roberta.csv")
     print(df.columns)
     df["cleaned"] = df["qa_company_1"].map(clean_str)
     df["cleaned"] = df["cleaned"].apply(lambda x: filter_states_info(x, states))
     df["cleaned"] = df["cleaned"].map(filter_company_name)
-    companies = list(df["cleaned"].unique())
 
+    companies = list(df["cleaned"].unique())
+    # companies = companies[:100]  # small test
     starting_size = len(companies)
 
     print("Length of cleaned companies: %s" % len(companies))
-    for i in tqdm(range(len(companies))):
-        for j in range(len(companies)):
-            if i != j and fuzz.ratio(companies[i], companies[j]) > 80:
-                found, name = non_normal_majority_merging(companies[i], companies[j])
-                if found:
-                    print(
-                        "merge found: (%s, %s) --> %s"
-                        % (companies[i], companies[j], name)
-                    )
-                    companies[i] = name
-                    companies[j] = name
 
-    print("List of merged companies: %s" % len(set(companies)))
-    print("Total companies reduced: %s" % (len(set(companies)) - starting_size))
+    print("Starting company matches")
+
+    company_dict = {
+        # company_name: set(different_company varaitions, variation2, variation3, ...)
+    }
+
+    start_length = len(companies)
+    with tqdm(total=start_length * start_length) as pbar:
+        while len(companies) > 0:
+            target_company = companies.pop(0)
+            company_dict[target_company] = [target_company]
+            pbar.set_description("Target company:\t%s" % target_company)
+
+            # now for all remaining companies, check if the company matches the company dictionary
+            for i in range(len(companies)):
+                candidate_company = companies.pop(0)
+                # print("candidate company: %s" % candidate_company)
+                # pbar.set_description("Checking:\t%s" % candidate_company)
+                matched = False
+                if fuzz.ratio(target_company, candidate_company) > 80:
+                    print(
+                        "Found candidate: (%s,%s) --> %s"
+                        % (
+                            target_company,
+                            candidate_company,
+                            check_same_name(target_company, candidate_company),
+                        )
+                    )
+                    if check_same_name(target_company, candidate_company):
+                        company_dict[target_company].append(candidate_company)
+                        print(company_dict[target_company])
+                        print(
+                            "\t[%s] new match found (%s,%s)"
+                            % (
+                                len(company_dict[target_company]),
+                                target_company,
+                                candidate_company,
+                            )
+                        )
+                        matched = True
+                if not matched:  # add it back into the original list
+                    companies.append(candidate_company)
+            pbar.update(start_length - len(companies))
+
+    pprint(company_dict)
+    reduced = pd.DataFrame.from_dict(company_dict, orient="index")
+    print(reduced)
+    reduced.to_csv("company_rep.csv", index=False)
